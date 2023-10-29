48efad783f6839db1672faaf743a6d42dcd5a96b
Izotop82 <142840997+Izotop82@users.noreply.github.com>
2023-10-29 11:06:15 +0300

    Add files via upload

--- Andrey_Voronin/track.py
+++ Andrey_Voronin/track.py
@@ -1,17 +1,1 @@
-from ultralytics import YOLO
-import torch
-
-
-def Track(file):
-    # Load the model.
-    model = YOLO('models/smokeroboflow_yolov8s_640_best.pt')
-    # model.to('cuda')
-    rez=model.predict(file, save=False, conf=0.45)
-    print('______________________________')
-
-
-    out =[i+1 for i,r in enumerate (rez) if len(r.boxes.cls)>0]
-    return out
-
-
-print('CUDA -',torch.cuda.is_available())
+#ЭТОТ ФАЙЛ МОЖНО УДАЛИТЬ
